8ae7c544752667a230e2dd4503fc82eb0ffd6bfb
boutayna <boutayna.lamrani@centrale-marseille.fr>
2013-01-15 16:38:05 +0100

    add splash screen

--- gen/com/example/monappli/R.java
+++ gen/com/example/monappli/R.java
@@ -25,6 +25,7 @@ public final class R {
         public static final int iconcarte=0x7f02000b;
         public static final int iconfavoris=0x7f02000c;
         public static final int iconliste=0x7f02000d;
+        public static final int loading=0x7f02000e;
     }
     public static final class id {
         public static final int addFavoris=0x7f07000f;
@@ -34,16 +35,18 @@ public final class R {
         public static final int detail=0x7f070004;
         public static final int favoris=0x7f070003;
         public static final int image=0x7f070000;
+        public static final int imageView1=0x7f070012;
         public static final int infos=0x7f070009;
         public static final int inputSearch=0x7f070011;
         public static final int linearLayout1=0x7f070007;
         public static final int listView1=0x7f070006;
         public static final int map=0x7f070010;
-        public static final int menu_settings=0x7f070012;
+        public static final int menu_settings=0x7f070014;
         public static final int name=0x7f070008;
         public static final int quartier=0x7f070005;
         public static final int sec=0x7f07000d;
         public static final int secteur=0x7f070001;
+        public static final int textView1=0x7f070013;
         public static final int tiret=0x7f07000c;
         public static final int yaller=0x7f070002;
     }
@@ -57,6 +60,7 @@ public final class R {
         public static final int json_layout=0x7f030006;
         public static final int map_layout=0x7f030007;
         public static final int mylistview=0x7f030008;
+        public static final int splash_screen=0x7f030009;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
